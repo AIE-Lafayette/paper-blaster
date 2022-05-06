6cb821c61d941ee452a620b55a2a7e1f3fc43b3c
Miles <miles.gr.lee@gmail.com>
2022-05-06 10:46:15 -0500

    Comments to spawning asteroids

--- Assets/Scripts/GameManagerBehavior.cs
+++ Assets/Scripts/GameManagerBehavior.cs
@@ -18,17 +18,21 @@ public class GameManagerBehavior : MonoBehaviour
 
     void Start()
     {
+        //For testing only
         SpawnAsteroids(5);
     }
 
     void Update()
     {
+        //If the difficulty threshold is more than the max difficulty threshold
         if (_difficultyThreshold > _difficultyThresholdMax) 
         {
+            //Increase difficulty
             _difficultyThreshold = 0;
             _difficultyThresholdMax++;
             _difficulty++;
         }
+        //Check for asteroids
         AsteroidCheck();
     }
 
@@ -40,8 +44,10 @@ public class GameManagerBehavior : MonoBehaviour
 
     void SpawnAsteroids(int amount) 
     {
+        //Spawn the given amount of asteroids
         for (int i = 0; i < amount; i++) 
         {
+            //Spawn an asteroid in a random position and add it to the list of asteroids
             Vector2 pos = RandomPointOnPerimeter(0, 0, _rectCornerX, _rectCornerZ);
             GameObject spawn = Instantiate(_asteroid, new Vector3(pos.x, 0, pos.y), Quaternion.identity);
             _asteroids.Add(spawn);
@@ -50,11 +56,13 @@ public class GameManagerBehavior : MonoBehaviour
 
     void AsteroidCheck() 
     {
+        //Check if destroyed asteroids to remove them from the list
         for (int i = 0; i < _asteroids.Count; i++) 
         {
             if(_asteroids[i] == null)
                 _asteroids.RemoveAt(i);
         }
+        //If there are no more asteroids
         if (_asteroids.Count == 0)
         {
             Debug.Log("All asteroids cleared.");
@@ -63,23 +71,28 @@ public class GameManagerBehavior : MonoBehaviour
 
     Vector2 RandomPointOnPerimeter(float x1, float y1, float x2, float y2) 
     {
+        //Pick a random side of the rectangle
         Vector2 point = new Vector2();
         int side = Random.Range(0, 4);
+        //Bottom side
         if (side == 0)
         {
             point.x = Random.Range(0, x2);
             point.y = y1;
         }
+        //Top side
         if (side == 1)
         {
             point.x = Random.Range(0, x2);
             point.y = y2;
         }
+        //Left side
         if (side == 2)
         {
             point.x = x1;
             point.y = Random.Range(0, y2);
         }
+        //Right side
         if (side == 3)
         {
             point.x = x2;
