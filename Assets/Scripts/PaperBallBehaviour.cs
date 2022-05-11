f5216090ad898013fe12b27888ca9019f159d0b8
Devin <devin.ray.broussard@gmail.com>
2022-05-11 17:02:00 -0500

    Remaking paperballbehaviour

--- Assets/Scripts/PaperBallBehaviour.cs
+++ Assets/Scripts/PaperBallBehaviour.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class PaperBallBehaviour : MonoBehaviour
 {
+    Rigidbody _rigidbody;
+
     [SerializeField]
     //A reference to paper balls
     private GameObject _paperBall;
-
-    //The time that the paper ball was spawned
-    private float _spawnTime;
-
-    //The cooldown for breaking into more paper balls
-    private float _breakCooldown;
-
     
     //The size variations of paper balls
     private enum PaperBallSize { Small, Medium, Large }
@@ -22,16 +17,26 @@ public class PaperBallBehaviour : MonoBehaviour
     //The size of this paper ball
     private PaperBallSize Size { get => _size; set => _size = value; }
 
+    private Vector3 _moveDirection;
+    [SerializeField]
+    private float _moveSpeed;
+    [SerializeField]
+    private float _maxSpeed;
 
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
 
     //Called when the paper ball is added to a scene
     private void Start()
     {
-        UpdateScale();
+        ApplyScale();
+        ApplyVelocity();
     }
 
     //Updates the paper ball's scale based on 
-    private void UpdateScale()
+    private void ApplyScale()
     {
         switch (Size)
         {
@@ -47,7 +52,7 @@ public class PaperBallBehaviour : MonoBehaviour
             }
             case PaperBallSize.Large:
             {
-                transform.localScale = new Vector3(1, 1, 1);
+                transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
                 break;
             }
             default:
@@ -56,6 +61,22 @@ public class PaperBallBehaviour : MonoBehaviour
                 break;
             }
         }
+    }
+
+    //Gives this paper ball a velocity
+    private void ApplyVelocity()
+    {
+        //Gives the ball a random direction
+        _moveDirection = new Vector3(Random.Range(-500.0f, 500.0f), 0, Random.Range(-500.0f, 500.0f)).normalized;
+
+        //Move in the correct direction scaled up by the move speed
+        _rigidbody.AddForce(_moveDirection * _moveSpeed * Time.deltaTime, ForceMode.Impulse);
+
+
+        //If the object is moving above the max speed
+        if (_rigidbody.velocity.magnitude > _maxSpeed)
+            //Set the velocity to be the max speed
+            _rigidbody.velocity = _rigidbody.velocity.normalized * _maxSpeed;
     }
 
     //Called upon collision with other objects
@@ -67,6 +88,8 @@ public class PaperBallBehaviour : MonoBehaviour
             case "Player": 
             {
                 Break();
+                //Award the player with points
+                Destroy(gameObject);
                 break;
             }
             case "PlayerBullet":
@@ -74,6 +97,7 @@ public class PaperBallBehaviour : MonoBehaviour
                 if (!Break())
                 {
                     //Award the player with points here
+                    Destroy(gameObject);
                 }
                 break;
             }
@@ -85,6 +109,8 @@ public class PaperBallBehaviour : MonoBehaviour
         //If the paper ball can't be broken, return
         if (Size == PaperBallSize.Small) return false;
 
+        PaperBallMovementBehaviour movementBehaviour = GetComponent<PaperBallMovementBehaviour>();
+
         //Creates two smaller paper balls
         for (int i = 0; i < 2; i++)
         {
@@ -93,12 +119,8 @@ public class PaperBallBehaviour : MonoBehaviour
 
             PaperBallBehaviour paperBallBehaviour = paperBall.GetComponent<PaperBallBehaviour>();
             paperBallBehaviour.Size = Size - 1;
-
-            PaperBallMovementBehaviour movementBehaviour = GetComponent<PaperBallMovementBehaviour>();
-            PaperBallMovementBehaviour newMovementBehaviour = paperBall.GetComponent<PaperBallMovementBehaviour>();
-            newMovementBehaviour.PreviousDirection = movementBehaviour.MoveDirection;
-            newMovementBehaviour.MoveSpeed *= 5;
-            newMovementBehaviour.MaxSpeed *= 5;
+            paperBallBehaviour._moveSpeed = 30;
+            paperBallBehaviour._rigidbody.velocity = _rigidbody.velocity;
         }
 
         return true;
--- Assets/Scripts/PaperBallMovementBehaviour.cs
+++ Assets/Scripts/PaperBallMovementBehaviour.cs
@@ -2,29 +2,27 @@ using UnityEngine;
 
 public class PaperBallMovementBehaviour : MovementBehavior
 {
-    private Vector3 _previousDirection;
-    
-    //The direction that the previous Paper Ball was moving in
-    public Vector3 PreviousDirection { get => _previousDirection; set => _previousDirection = value; }
-
+    //The slowest speed that the paper ball can move at
+    float _minSpeed;
 
     //Called when this object is added to a scene
-    void Start()
+    private void Start()
     {
-        //If this paper ball was assigned with a previous direction...
-        if (PreviousDirection.z != 0 && PreviousDirection.x != 0) 
-        {
-            //Offset that direction and make that the new move direction
-            Vector3 offset = new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5)).normalized;
-            MoveDirection = (PreviousDirection + offset).normalized;
-        }
-        else
-        {
-            //Otherwise, give the paper ball a random direction
-            MoveDirection = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100)).normalized;
-        }
-
+       
         Move();
+    }
+
+    public override void Move()
+    {
+        //Gets the paper ball's rigidbody and calls the base move function
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        base.Move();
+
+        //Clamps the paper ball's speed
+        if (rigidbody.velocity.magnitude < _minSpeed)
+        {
+            rigidbody.velocity = (rigidbody.velocity.normalized * _minSpeed);
+        }
     }
 
 }
