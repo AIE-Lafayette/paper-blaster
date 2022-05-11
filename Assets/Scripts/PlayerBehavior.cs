b73e8adee35efc3c542bb3f8c71f60298038a857
Peyton <peytonsemail2000@gmail.com>
2022-05-11 14:30:11 -0500

    Added I-Frame timer to player behavior

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -7,6 +7,7 @@ public class PlayerBehavior : MonoBehaviour
 {
     private HealthBehavior _health;
     private Rigidbody _player;
+    private float _timer = 0;
 
     // Start is called before the first frame update
     void Awake()
@@ -24,18 +25,31 @@ public class PlayerBehavior : MonoBehaviour
             onDeath();
     }
 
+    /// <summary>
+    /// Called when the player runs out of lives
+    /// </summary>
     private void onDeath()
     {
         Destroy(gameObject);
     }
 
+    /// <summary>
+    /// Called when the player collides with some object
+    /// </summary>
+    /// <param name="other">The other object</param>
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "PaperBall")
         {
-            _health.CurrentHealth--;
+            //Timer of i-frames
+            if (Time.time - _timer < 3)
+                return;
+            //Takes damage and resets player to the middle
+            _health.TakeDamage(1);
             _player.position = new Vector3(0f, .5f, 0f);
             _player.velocity = Vector3.zero;
+            //Sets the timer to the current time to reset timer
+            _timer = Time.time;
         }
 
     }
