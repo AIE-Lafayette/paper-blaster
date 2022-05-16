eaf934b07eca4e7af3f83ced57bd1b3f774d7cb9
Miles <miles.gr.lee@gmail.com>
2022-05-16 11:17:23 -0500

    Merge peyton timer

---
+++ Assets/Scripts/PlayerShootingBehavior.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootingBehavior : MonoBehaviour
+{
+    [SerializeField] private GameObject _projectile;
+    [SerializeField] private float _attackSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
