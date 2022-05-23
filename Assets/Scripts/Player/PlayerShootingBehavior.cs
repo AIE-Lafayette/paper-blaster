e25074b5918048c83dd1545d1c62942225e2ff86
Peyton <peytonsemail2000@gmail.com>
2022-05-23 12:30:09 -0500

    Triple Shot works, paper balls have bugs
    If multiple bullets hit one paper ball the spawn is doubled

--- Assets/Scripts/Player/PlayerShootingBehavior.cs
+++ Assets/Scripts/Player/PlayerShootingBehavior.cs
@@ -30,6 +30,9 @@ public class PlayerShootingBehavior : MonoBehaviour
 
     public void Shoot() 
     {
+        Quaternion angleChange2 = Quaternion.Euler(_bulletPoint.eulerAngles.x, _bulletPoint.eulerAngles.y + 20, _bulletPoint.eulerAngles.z);
+        Quaternion angleChange3 = Quaternion.Euler(_bulletPoint.eulerAngles.x, _bulletPoint.eulerAngles.y - 20, _bulletPoint.eulerAngles.z);
+
         if (_readyToAttack) 
         {
             if (_currentPowerup == "TripleShotPowerup")
@@ -37,10 +40,10 @@ public class PlayerShootingBehavior : MonoBehaviour
                 _readyToAttack = false;
                 Rigidbody bullet = Instantiate(_projectile, _bulletPoint.position, _bulletPoint.rotation).GetComponent<Rigidbody>();
                 bullet.AddForce(bullet.transform.forward * 1000);
-                Rigidbody bullet2 = Instantiate(_projectile, _bulletPoint.position, angleChange).GetComponent<Rigidbody>();
-
-                //bullet2.transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, new Vector3(0, 30, 0), Time.deltaTime);
+                Rigidbody bullet2 = Instantiate(_projectile, _bulletPoint.position, angleChange2).GetComponent<Rigidbody>();
                 bullet2.AddForce(bullet2.transform.forward * 1000);
+                Rigidbody bullet3 = Instantiate(_projectile, _bulletPoint.position, angleChange3).GetComponent<Rigidbody>();
+                bullet3.AddForce(bullet3.transform.forward * 1000);
                 RoutineBehaviour.Instance.StartNewTimedAction(args => _readyToAttack = true, TimedActionCountType.SCALEDTIME, _attackSpeed);
             }
             else
