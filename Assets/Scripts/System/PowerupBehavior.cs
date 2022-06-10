e6c50f13a90452f78ff0349e67b4aa1a2713e5e1
Peyton <peytonsemail2000@gmail.com>
2022-06-10 10:20:28 -0500

    Auto stash before merge of "master" and "Github/master"

--- Assets/Scripts/System/PowerupBehavior.cs
+++ Assets/Scripts/System/PowerupBehavior.cs
@@ -13,7 +13,7 @@ public class PowerupBehavior : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && !PlayerShootingBehavior.PowerupActive)
         {
             PlayerShootingBehavior.CurrentPowerupHeld = tag;
             Destroy(gameObject);
